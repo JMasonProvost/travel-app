2b212d078e8d01a903d436d9a28f87404e941bb6
Julie Provost <juliemasonprovost@gmail.com>
2023-01-18 09:01:58 -0600

    e added

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-const makeRandColor = () => {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    return `rgb(${r}, ${g}, ${b}`;
-
-}
-const buttons = document.querySelectorAll('button');
-
-for (let button of buttons) {
-    button.addEventListener('click', function () {
-        button.style.backgroundColor = makeRandColor();
-
-    })
-}
+const form = document.querySelector("#shelterForm");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("SUBMITTED!!!");
+});
